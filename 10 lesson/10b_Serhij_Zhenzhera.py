dc95a3479a5c40affd0b79a2eccb8e00d761d816
Serhij Zhenzhera <73667238+SerhijZhenzhera@users.noreply.github.com>
2021-01-08 22:30:15 +0200

    home work 10b

---
+++ 10 lesson/10b_Serhij_Zhenzhera.py
@@ -0,0 +1,19 @@
+'''
+Task 2. Doggy age
+Create a class Dog with class attribute `age_factor` equals to 7.
+Make __init__() which takes values for a dog’s age. Then create a method `human_age` which returns the dog’s age in human equivalent.
+'''
+
+
+class Dog:
+    age_factor = 7
+
+    def __init__(self, d_a):
+        self.dog_age = d_a
+
+    def human_age(self):
+        return self.dog_age*Dog.age_factor
+
+
+d1 = Dog(3)
+print(d1.human_age())  # test
