3f3216299a56b4eb0346c22ea190c1d6808446f0
Serhij Zhenzhera <73667238+SerhijZhenzhera@users.noreply.github.com>
2021-01-12 17:54:26 +0200

    home work 10b fixed 2

--- 10 lesson/10b_Serhij_Zhenzhera_fixed.py
+++ 10 lesson/10b_Serhij_Zhenzhera_fixed.py
@@ -12,8 +12,17 @@ class Dog:
         self.dog_age = d_a
 
     def human_age(self):
-        return self.dog_age*self.age_factor
+        return self.dog_age*self.age_factor # лучше Dog.age_factor: можно дать индивидуальный атрибут d1.age_factor
 
 
 d1 = Dog(3)
 print(d1.human_age())  # test
+d2 = Dog(3)
+d2.age_factor = 5
+print(d2.human_age()) # test
+
+'''
+---output---
+21
+15
+'''
