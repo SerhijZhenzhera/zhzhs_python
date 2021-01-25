2b2edf7f919a67572612f110dc44a0ccbaf4c75a
Serhij Zhenzhera <73667238+SerhijZhenzhera@users.noreply.github.com>
2021-01-25 16:34:20 +0200

    16b and 16c

---
+++ 16 leson/16b_Serhij_Zhenzhera.py
@@ -0,0 +1,60 @@
+'''
+Task 2. Create your own implementation of a built-in function range, named in_range(),
+which takes three parameters: `start`, `end`, and optional step.
+Tips: See the documentation for `range` function
+'''
+
+'''
+def in_range_result(start, end, step=1):
+    result = []
+    while start <= end:
+        result.append(start)
+        start += step
+    return result
+'''
+
+
+def in_range(*args):
+    temp = []
+    i = 0
+    for arg in args:
+        if not type(arg) == int:
+            text = 'Only numbers!'
+            return text
+        else:
+            temp.append(arg)
+            i += 1
+    if i < 2:
+        text = 'Add the "end" arg, please!'
+        return text
+    elif i > 3:
+        text = 'Two or three args only!'
+        return text
+    else:
+        def in_range_result(start, end, step=1):
+            result = []
+            while start <= end:
+                result.append(start)
+                start += step
+            return result
+        return in_range_result(*temp)
+
+
+
+if __name__ == "__main__":
+    print(in_range(1, 12))
+    print(in_range(1, 12, 3))
+    print(in_range(1, 12, 3, 5))
+    print(in_range(1, '5'))
+    print(in_range(1))
+    
+
+
+'''
+---output---
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+[1, 4, 7, 10]
+Two or three args only!
+Only numbers!
+Add the "end" arg, please!
+'''
