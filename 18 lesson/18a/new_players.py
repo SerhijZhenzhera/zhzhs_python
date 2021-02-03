565381adae1a3657ff225526ed617488fb530ca9
Serhij Zhenzhera <73667238+SerhijZhenzhera@users.noreply.github.com>
2021-02-03 14:27:27 +0200

    new players reload

--- 18 lesson/18a/new_players.py
+++ 18 lesson/18a/new_players.py
@@ -4,7 +4,7 @@ Imports practice
 Make a directory with 2 modules; make a function in one of them; then import this function in the other module and use that in your script of choice.
 '''
 
-from positions import cf
+from positions import cf as forward_cnt
 
 
 pl_b_22 = {'attack': 125, 'middle': 90, 'defence': 70,
@@ -12,7 +12,7 @@ pl_b_22 = {'attack': 125, 'middle': 90, 'defence': 70,
 
 
 if __name__ == "__main__":
-    print(cf(pl_b_22))
+    print(forward_cnt(pl_b_22))
 
 
 '''
