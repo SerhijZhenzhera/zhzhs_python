4ce1bb135f6b51f03a4e1e4f59957f177fe77243
Serhij Zhenzhera <73667238+SerhijZhenzhera@users.noreply.github.com>
2021-02-03 11:13:39 +0200

    home work 18

---
+++ new_players.py
@@ -0,0 +1,21 @@
+'''
+Task 1
+Imports practice
+Make a directory with 2 modules; make a function in one of them; then import this function in the other module and use that in your script of choice.
+'''
+
+from positions import cf
+
+
+pl_b_22 = {'attack': 125, 'middle': 90, 'defence': 70,
+           'keeper': 10, 'left': 0.75, 'right': 1.5, 'center': 1.4}
+
+
+if __name__ == "__main__":
+    print(cf(pl_b_22))
+
+
+'''
+---output---
+[175.0]
+'''
